96dff1efc027d0b38bff9394703984618ad5438f
João Alexandre Bezerra da Silva <joao.alexandre@fariasbrito.com.br>
2024-11-21 17:52:39 -0300

    teste

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {Container, Content} from './style';
+
+function App()  {
+  return (
+      <Container>
+        <Content>
+          Ola 
+        </Content>
+      </Container>
+  )
+}
+
+export default App;
+
---
+++ src/style.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+    width: 100%;
+    height: 100vh;
+    background-color: #CACACA;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Content = styled.section`
+    background-color: #FFFFFF;
+    width: 80%
+    min-height: 350px
+`
